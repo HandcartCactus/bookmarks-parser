39ec2436420ef58b50f16244843d43544deae6b0
EJ <38086802+HandcartCactus@users.noreply.github.com>
2024-03-28 01:18:37 -0400

    Create test_parse_flat.py

---
+++ tests/test_parse_flat.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+import pytest
+import bookmarks_parser
+
+tests_data_path = (os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tests_data'))
+
+
+@pytest.mark.parametrize("input_path, result_path", [('chrome_bookmarks.html', 'chrome_bookmarks_flat.json'),
+                                                     ('firefox_bookmarks.html', 'firefox_bookmarks_flat.json')])
+def test_parser(input_path, result_path):
+    input_path = os.path.join(tests_data_path, input_path)
+    result_path = os.path.join(tests_data_path, result_path)
+    with open(result_path, 'r') as f:
+        result = json.load(f)
+    assert bookmarks_parser.parse_flat(input_path) == result
